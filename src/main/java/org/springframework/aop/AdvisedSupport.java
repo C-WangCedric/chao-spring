002709d2b9bde917437c5b1f3475fd611b38c374
王超 <569110634@qq.com>
2023-06-30 15:37:33 +0800

    dynamic-proxy

---
+++ src/main/java/org/springframework/aop/AdvisedSupport.java
@@ -0,0 +1,48 @@
+package org.springframework.aop;
+
+
+import org.aopalliance.intercept.MethodInterceptor;
+
+/**
+ * @author WangChao
+ * @version 1.0
+ * @date 2023/6/30 10:49
+ */
+public class AdvisedSupport {
+    /**
+     * 被代理对象
+     */
+    private TargetSource targetSource;
+    /**
+     * 方法拦截器是一种在方法调用前后进行额外逻辑处理的机制
+     */
+    private MethodInterceptor methodInterceptor;
+    /**
+     * 方法匹配
+     */
+    private MethodMatcher methodMatcher;
+
+    public TargetSource getTargetSource(){
+        return this.targetSource;
+    }
+
+    public void setTargetSource(TargetSource targetSource){
+        this.targetSource = targetSource;
+    }
+
+    public MethodInterceptor getMethodInterceptor(){
+        return this.methodInterceptor;
+    }
+
+    public void setMethodInterceptor(MethodInterceptor methodInterceptor){
+        this.methodInterceptor = methodInterceptor;
+    }
+
+    public MethodMatcher getMethodMatcher(){
+        return this.methodMatcher;
+    }
+
+    public void setMethodMatcher(MethodMatcher methodMatcher){
+        this.methodMatcher = methodMatcher;
+    }
+}
---
+++ src/test/java/org/springframework/test/service/WorldService.java
@@ -0,0 +1,10 @@
+package org.springframework.test.service;
+
+/**
+ * @author WangChao
+ * @version 1.0
+ * @date 2023/6/30 11:35
+ */
+public interface WorldService {
+    void explode();
+}
